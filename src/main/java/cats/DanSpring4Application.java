50e0aeb5012128ee7ad299232e93c3ce8f039c5c
Danny Smith <danny@atomist.com>
2018-10-09 16:50:38 +0100

    Initial commit from Atomist

---
+++ src/main/java/cats/DanSpring4Application.java
@@ -0,0 +1,12 @@
+package cats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanSpring4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanSpring4Application.class, args);
+	}
+}
